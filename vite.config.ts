2ff3214832259af0b900d58e38c1d8f400ad7f72
ваше_имя <_email@.com>
2025-04-30 15:27:25 +0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/eka_consult/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
